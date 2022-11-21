3b51e1b59df429a4d8077a2560c64a8d58ceb9f2
BOB450 <bwvoskuil@gmail.com>
2022-11-21 11:26:04 -0700

    upadted modpack

--- modpack/overrides/kubejs/server_scripts/recipetypes/modern_industrialization/macerator.js
+++ modpack/overrides/kubejs/server_scripts/recipetypes/modern_industrialization/macerator.js
@@ -4,23 +4,70 @@
 
 ServerEvents.recipes(event => {
 
-  
-    event.custom({    
-        "type": "modern_industrialization:macerator",
-        "id": "aof:mi_macerator_pervaded_netherrack",
-        "eu": 2,
-        "duration": 100,
-        "item_inputs": [
-          {
-            "item": "byg:pervaded_netherrack",
-            "amount": 1
-          }
-        ],
-        "item_outputs": [
-          {
-            "item": "minecraft:glowstone_dust",
-            "amount": 2
-          }
-        ]
+
+  event.custom({
+    "type": "modern_industrialization:macerator",
+    "id": "aof:mi_macerator_pervaded_netherrack",
+    "eu": 2,
+    "duration": 200,
+    "item_inputs": [
+      {
+        "item": "byg:pervaded_netherrack",
+        "amount": 1
+      }
+    ],
+    "item_outputs": [
+      {
+        "item": "minecraft:glowstone_dust",
+        "amount": 2
+      }
+    ]
+  })
+
+  event.custom({
+    "type": "modern_industrialization:macerator",
+    "id": "aof:mi_macerator_crushed_zinc_ore",
+    "eu": 2,
+    "duration": 200,
+    "item_inputs": [
+      {
+        "tag": "c:ores/zinc",
+        "amount": 1
+      }
+    ],
+    "item_outputs": [
+      {
+        "item": "create:crushed_zinc_ore",
+        "amount": 2
+      }
+    ]
+  })
+
+  const spectrum = [
+    'topaz',
+    'amethyst',
+    'citrine',
+    'onyx'
+];
+
+  spectrum.forEach((item) => { 
+  event.custom({
+    "type": "modern_industrialization:macerator",
+    "eu": 2,
+    "duration": 200,
+    "item_inputs": [
+      {
+        "tag": "spectrum:" + item + "_ores",
+        "amount": 1
+      }
+    ],
+    "item_outputs": [
+      {
+        "item": "spectrum:" + item + "_powder",
+        "amount": 16
+      }
+    ]
+  })
+});
+
 })
-    })
--- modpack/overrides/kubejs/server_scripts/tags/general_tags.js
+++ modpack/overrides/kubejs/server_scripts/tags/general_tags.js
@@ -14,18 +14,16 @@ ServerEvents.tags('item', event => {
   event.add('c:zinc_plates', 'techreborn:zinc_plate')
   event.add('c:brass_plates', 'techreborn:brass_plate')
 
-  /* 
-  event.add('c:gold_plates', 'create:golden_sheet')
-  event.add('c:iron_plates', 'create:iron_sheet')
-  event.add('c:copper_plates', 'create:copper_sheet')
-  */
-
-  // Indrev
-
   event.add('c:raw_lead_blocks', 'indrev:raw_lead_block')
   event.add('c:raw_silver_blocks', 'indrev:raw_silver_block')
   event.add('c:raw_tungsten_blocks', 'indrev:raw_tungsten_block')
   event.add('c:raw_tin_blocks', 'indrev:raw_tin_block')
+});
+
+  ServerEvents.tags('block', event => {
+
+  // Graves fix
+  event.add('minecells:conjunctivius_unbreakable', 'yigd:grave')
 
 
   
